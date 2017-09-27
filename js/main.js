c118a26e39fcb1074a6c9e06ab64d9d0fd9a9fc8
Maria Dulatova <maria_dulatova@mail.ru>
2017-09-27 14:07:59 +0300

    Завершена стилизация без адаптива

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.new__list').slick({
+  slidesToShow: 4
+});
+
+$('.latest__list').slick({
+  slidesToShow: 4
+});
+
+$('.featured__list').slick({
+//  slidesToShow: 4,
+  rows: 2,
+  slidesPerRow: 3,
+});
+
+$('.hot-deals__list').slick({
+//  slidesToShow: 1
+});
+
+$('.special-deal__list').slick({
+//  slidesToShow: 1
+});
