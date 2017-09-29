b42c13fbe9c457f3c47872762e44c836d3f44825
Maria Dulatova <maria_dulatova@mail.ru>
2017-09-29 14:35:04 +0300

    Завершена верстка и стилизация страницы (адаптив)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,98 @@
+// Скрытие мобильного меню при клике на ссылку
+var menuLinks = document.querySelectorAll('.header-nav__link');
+
+for (var i = 0; i < menuLinks.length; i++) {
+  var menuLink = menuLinks[i];
+  var menuToggle = document.querySelector('#header-nav__toggle');
+  menuLink.addEventListener('click', function() {
+    menuToggle.checked = false;
+  })
+}
+
+// Скрытие меню с основными категориями при клике на Categories
+var categoriesLinks = document.querySelectorAll('.main-menu__link');
+
+for (var i = 0; i < categoriesLinks.length; i++) {
+  var categoriesLink = categoriesLinks[i];
+  var categoriesToggle = document.querySelector('#main-menu__toggle');
+  categoriesLink.addEventListener('click', function() {
+    categoriesToggle.checked = false;
+  })
+}
+
+
 $('.new__list').slick({
-  slidesToShow: 4
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
 $('.latest__list').slick({
-  slidesToShow: 4
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
 $('.featured__list').slick({
-//  slidesToShow: 4,
   rows: 2,
   slidesPerRow: 3,
+  responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        rows: 2,
+        slidesPerRow: 2
+      }
+    },
+    
+    {
+      breakpoint: 530,
+      settings: {
+        rows: 2,
+        slidesPerRow: 1
+      }
+    }
+  ]
 });
 
 $('.hot-deals__list').slick({
